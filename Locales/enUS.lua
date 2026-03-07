bbb0bfd01316703067a477b638ad08727ade84a0
GrazyMonster <monster@diegamer.com>
2026-03-07 22:49:08 +0100

    Update enUS.lua
    Add enUS.lua

---
+++ Locales/enUS.lua
@@ -0,0 +1,39 @@
+-- HarvestGoal - English Localization (Fallback)
+
+local addonName = ...
+local HG = _G[addonName]
+
+HG.L = HG.L or {}
+local L = HG.L
+
+------------------------------------------------------------
+-- General
+------------------------------------------------------------
+
+L["TITLE"] = "HarvestGoal"
+
+------------------------------------------------------------
+-- Minimap Tooltip
+------------------------------------------------------------
+
+L["MINIMAP_LEFT"]  = "Left Click: Toggle Window"
+L["MINIMAP_RIGHT"] = "Right Click: Options"
+L["MINIMAP_DRAG"]  = "Shift + Drag: Move"
+
+------------------------------------------------------------
+-- Context Menu
+------------------------------------------------------------
+
+L["MENU_HORIZONTAL"] = "Horizontal Layout"
+L["MENU_VERTICAL"]   = "Vertical Layout"
+
+L["MENU_LOCK"]   = "Lock Window"
+L["MENU_UNLOCK"] = "Unlock Window"
+
+L["MENU_RESET"] = "Reset Position"
+
+------------------------------------------------------------
+-- Goal Popup
+------------------------------------------------------------
+
+L["GOAL_SET"] = "Set Goal"
