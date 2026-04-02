e9052303dd268e64c7a674f64a5f7b702a02d6f7
GrazyMonster <monster@diegamer.com>
2026-04-02 22:31:30 +0200

    Update enUS.lua
    Update en/US

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -11,12 +11,12 @@ local L = HG.L
 ------------------------------------------------------------
 
 L["TITLE"] = "HarvestGoal"
-L["TITLE_SECOND"] = "HarvestGoal 2"
+
 ------------------------------------------------------------
 -- Minimap Tooltip
 ------------------------------------------------------------
 
-L["MINIMAP_LEFT"]  = "Left Click: Toggle Window"
+L["MINIMAP_LEFT"]  = "Left Click: Toggle all windows"
 L["MINIMAP_RIGHT"] = "Right Click: Options"
 L["MINIMAP_DRAG"]  = "Shift + Drag: Move"
 
@@ -24,38 +24,30 @@ L["MINIMAP_DRAG"]  = "Shift + Drag: Move"
 -- Context Menu
 ------------------------------------------------------------
 
-L["MENU_HORIZONTAL"] = "Horizontal Layout"
-L["MENU_VERTICAL"]   = "Vertical Layout"
+L["MENU_NEW_WINDOW"]       = "Create New Window"
+L["MENU_RENAME_WINDOW"]    = "Rename Window"
 
-L["MENU_LOCK"]   = "Lock Window"
-L["MENU_UNLOCK"] = "Unlock Window"
+L["MENU_HORIZONTAL"]       = "Horizontal Layout"
+L["MENU_VERTICAL"]         = "Vertical Layout"
 
-L["MENU_RESET"] = "Reset Position"
+L["MENU_LOCK"]             = "Lock Window"
+L["MENU_UNLOCK"]           = "Unlock Window"
 
-------------------------------------------------------------
--- Goal Popup
-------------------------------------------------------------
+L["MENU_ADD_SLOT"]         = "Add Slot"
+L["MENU_REMOVE_SLOT"]      = "Remove Slot"
 
-L["GOAL_SET"] = "Set Goal"
-L["SET_GOAL"] = "Set Goal"
+L["MENU_HIDE_WINDOW"]      = "Hide Window"
+L["MENU_SHOW_WINDOW"]      = "Show Window"
+L["MENU_DELETE_WINDOW"]    = "Delete Window"
 
-L["TITLE"] = "HarvestGoal"
-L["SET_GOAL"] = "Set Goal"
-L["RENAME_WINDOW"] = "Rename Window"
-
-L["MENU_HORIZONTAL"] = "Horizontal"
-L["MENU_VERTICAL"] = "Vertical"
-L["MENU_LOCK"] = "Lock Window"
-L["MENU_UNLOCK"] = "Unlock Window"
-L["MENU_RESET"] = "Reset Position"
-
-L["MENU_NEW_WINDOW"] = "Create New Window"
-L["MENU_RENAME_WINDOW"] = "Rename Window"
-L["MENU_HIDE_WINDOW"] = "Hide Window"
-L["MENU_SHOW_WINDOW"] = "Show Window"
 L["MENU_SHOW_ALL_WINDOWS"] = "Show All Windows"
 L["MENU_HIDE_ALL_WINDOWS"] = "Hide All Windows"
-L["MENU_DELETE_WINDOW"] = "Delete Window"
 
-L["MENU_ADD_SLOT"] = "Add Slot"
-L["MENU_REMOVE_SLOT"] = "Remove Slot"
+L["MENU_RESET"]            = "Reset Position"
+
+------------------------------------------------------------
+-- Popups
+------------------------------------------------------------
+
+L["GOAL_SET"]      = "Set Goal"
+L["RENAME_WINDOW"] = "Rename Window"
